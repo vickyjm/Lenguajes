03ec41ad4b5ad9ec334a6b964a2079036243f855
jemd93 <jemd311@gmail.com>
2015-05-11 13:25:31 -0430

    SubImagen. hay que probar

--- ProyectoHaskell/mosaico-bin/src/Imagen.hs
+++ ProyectoHaskell/mosaico-bin/src/Imagen.hs
@@ -13,7 +13,12 @@ subImagen
   -> Integer -> Integer
   -> Imagen -> Imagen
 
-subImagen = undefined
+subImagen xIni yIni anchura' altura' imagen = (Imagen anchura' altura' (colores imagen))
+									where colores (Imagen x y cols) = if (xIni == 0) then elimCol (take (altura'-1) cols)
+																	  else elimCol (take (altura'-1) (drop (xIni-1) cols))
+
+										  elimCol m = if (yIni == 0) then map (take (anchura'-1) m)
+										  			  else map (take (anchura'-1)) (map (drop (yIni-1))
 
 
 
