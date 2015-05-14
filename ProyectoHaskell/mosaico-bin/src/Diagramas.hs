fe127f2179ce2ce7c12901c19ca4a9f9d99ab62f
jemd93 <jemd311@gmail.com>
2015-05-14 17:57:25 -0430

    Arreglado dividir...creo

--- ProyectoHaskell/mosaico-bin/src/Diagramas.hs
+++ ProyectoHaskell/mosaico-bin/src/Diagramas.hs
@@ -22,10 +22,6 @@ data Orientación
   deriving Show
 
 -- HOLA VICKY espero que te sientas mejor :D.
--- la parte de f1 y f2 esta dando un error de tipos por Hoja y no lo logro arreglar
--- Lo otro es que dice que devuelve Maybe Diagrama,pero no se que tipo de diagrama quiere 
--- asi que en principio estoy suponiendo que quiere dos hojas con lo que se dividio
--- pero no se si esa es la idea, o que.
 dividir :: Orientación -> Rectángulo -> Maybe Diagrama
 dividir o (Rectángulo prom (Imagen anch alt cols)) = case o of Horizontal -> 
                                                                   if alt < 2 then Nothing
@@ -33,8 +29,8 @@ dividir o (Rectángulo prom (Imagen anch alt cols)) = case o of Horizontal ->
                                                                Vertical ->
                                                                   if anch < 2 then Nothing
                                                                   else Just (f2 (vSplit (Imagen anch alt cols)))
-                                                where f1 (img1,img2) = ((Hoja (rectánguloImagen img1)) (:-:) (Hoja (rectánguloImagen img2)))
-                                                      f2 (img1,img2) = ((Hoja (rectánguloImagen img1)) (:|:) (Hoja (rectánguloImagen img2)))
+                                                where f1 (img1,img2) = ((Hoja (rectánguloImagen img1)) :-: (Hoja (rectánguloImagen img2)))
+                                                      f2 (img1,img2) = ((Hoja (rectánguloImagen img1)) :|: (Hoja (rectánguloImagen img2)))
 
 caminar :: [Paso] -> Diagrama -> Maybe Diagrama
 caminar = undefined
