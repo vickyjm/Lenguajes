308631263032bc51cefd790dab33686552b4b1e9
vickyjm <mvjorgemauriello@gmail.com>
2015-05-16 20:25:35 -0430

    Main parece listo

--- ProyectoHaskell/mosaico-bin/src/Main.hs
+++ ProyectoHaskell/mosaico-bin/src/Main.hs
@@ -5,9 +5,19 @@ import Graphics.Mosaico.Imagen   (Imagen(Imagen, altura, anchura, datos), leerIm
 import Graphics.Mosaico.Ventana  (Ventana, cerrar, crearVentana, leerTecla, mostrar)
 
 import Diagramas (Orientación(Horizontal, Vertical), caminar, dividir, rectánguloImagen, sustituir)
+import System.Environment (getArgs)
+import System.IO(stderr,hPutStrLn)
 
 ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
 ciclo = undefined
 
 main :: IO ()
-main = undefined
+main = do
+    args <- getArgs
+    if length args /= 1 then do hPutStrLn stderr "Ingrese una imagen"
+    else do
+        imagen <- leerImagen (head args)
+        case imagen of Left msg -> hPutStrLn stderr msg
+                       Right (Imagen ancho alto cols) -> do
+                                    vent <- crearVentana ancho alto 
+                                    ciclo vent (Hoja (rectánguloImagen (Imagen ancho alto cols))) []
