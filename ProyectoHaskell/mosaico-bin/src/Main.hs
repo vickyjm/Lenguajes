829a4d45f60484324ab770edb3c7c4421f4a800f
jemd93 <jemd311@gmail.com>
2015-05-17 18:03:26 -0430

    Falta arreglar lo de los colores y el otherwise

--- ProyectoHaskell/mosaico-bin/src/Main.hs
+++ ProyectoHaskell/mosaico-bin/src/Main.hs
@@ -16,31 +16,37 @@ extraerDiag :: Maybe Diagrama -> Diagrama
 extraerDiag (Just d) = d
 extraerDiag Nothing = Hoja (Rectángulo (Color 0 0 0) (Imagen 0 0 [[Color 0 0 0]]))
 
+checkVacio :: Diagrama -> Bool
+checkVacio d = case d of Hoja (Rectángulo (Color 0 0 0) (Imagen 0 0 [[Color 0 0 0]])) -> True
+                         otherwise -> False
+
 divAux :: String -> Diagrama -> Diagrama
 divAux tecla (Hoja r1) = case tecla of "Up"    -> (extraerDiag (dividir Horizontal r1))
                                        "Down"  -> (extraerDiag (dividir Horizontal r1))
                                        "Left"  -> (extraerDiag (dividir Vertical r1))
                                        "Right" -> (extraerDiag (dividir Vertical r1))
-                                       otherwise -> (Hoja r1)
+                                       otherwise ->  (Hoja r1)
 divAux _ d1 = d1
 
 
 ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
 ciclo v d ps = do
                 mostrar v ps d
-                teclaLeida <- leerTecla v          -- Maybe String guardado aqui
+                teclaLeida <- leerTecla v                                         -- Maybe String guardado aqui
                 let 
-                    tecla = (mostrarTecla teclaLeida)  -- Tecla guardada en tecla
-                    diag = (extraerDiag (caminar (reverse ps) d)) -- Obtener el subarbol sobre el cual vamos a trabajar
-                    diag2 = divAux tecla diag -- Divido si es hoja, dejo igual si no
-                    diag3 = sustituir (diag2) (reverse ps) d -- Sustituyo el resultado anterior en el diagrama original
-                case tecla of "Up"    -> ciclo v diag3 (Primero:ps)
-                              "Down"  -> ciclo v diag3 (Segundo:ps)
-                              "Left"  -> ciclo v diag3 (Primero:ps)
-                              "Right" -> ciclo v diag3 (Segundo:ps)
-                              "BackSpace" -> if null ps then ciclo v d ps else ciclo v d (tail ps)
-                              "q"     -> cerrar v
-                              otherwise -> ciclo v d ps
+                    tecla = (mostrarTecla teclaLeida)                             -- Tecla guardada en tecla
+                    diag = (extraerDiag (caminar (reverse ps) d))                 -- Obtener el subarbol sobre el cual vamos a trabajar
+                    diag2 = divAux tecla diag                                     -- Divido si es hoja, dejo igual si no
+                    diag3 = (sustituir (diag2) (reverse ps) d)
+                if (checkVacio diag2) then ciclo v d ps
+                else 
+                  case tecla of "Up"    -> ciclo v diag3 (Primero:ps)
+                                "Down"  -> ciclo v diag3 (Segundo:ps)
+                                "Left"  -> ciclo v diag3 (Primero:ps)
+                                "Right" -> ciclo v diag3 (Segundo:ps)
+                                "BackSpace" -> if null ps then ciclo v d ps else ciclo v d (tail ps)
+                                "q"     -> cerrar v
+                                otherwise -> ciclo v d ps
 
 main :: IO ()
 main = do
