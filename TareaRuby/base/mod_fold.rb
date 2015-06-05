306fc2e55372dda907d50c4e6d39c98215ef8e47
vickyjm <mvjorgemauriello@gmail.com>
2015-06-04 21:10:12 -0430

    Listo mod_fold

--- TareaRuby/base/mod_fold.rb
+++ TareaRuby/base/mod_fold.rb
@@ -1,11 +1,37 @@
 module Foldable
 
     def fold(b, &bloque)
+    	cola = [self]
+    	while (!cola.empty?)
+    		actual = cola.shift
+    		b = yield b,actual.n
+    		actual.each do |hijo|
+    			cola.push(hijo)
+    		end
+    	end
+    	return b
     end
 
     def map &block
+    	cola = [self]
+		while(!cola.empty?)
+			actual = cola.shift
+			yield actual
+			actual.each do |hijo|
+				cola.push(hijo)
+			end
+		end    	
     end
 
     def map! &block
+    	cola = [self]
+    	while (!cola.empty?)
+    		actual = cola.shift
+    		actual.n = yield actual
+    		actual.each do |hijo|
+    			cola.push(hijo)
+    		end
+    	end
     end
+
 end
