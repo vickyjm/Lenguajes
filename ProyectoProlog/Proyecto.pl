f2adb8d54d71753eb1c7e08b727852a3b5d77ce6
vickyjm <mvjorgemauriello@gmail.com>
2015-06-21 08:43:16 -0430

    Arreglado numColums

--- ProyectoProlog/Proyecto.pl
+++ ProyectoProlog/Proyecto.pl
@@ -11,8 +11,7 @@ numIguales([],_,_,_).
 numIguales([_|T],X,Y,Elem) :- X1 is X-1, X > 0, numIguales(T,X1,Y,Elem).
 numIguales([H|_],0,Y,Elem) :- Y1 is Y+1, nth(Y1,H,Elem).
 
-numFilas([],0).
-numFilas([_|T],Tam) :- numFilas(T,Tam1),Tam is Tam1+1.
+numColums([H|_],Tam) :- length(H,Tam).
 
 saltoEnPosicion(_,0,X,Y,solucion([H|[]],X1,Y1,[])).  %Caso base cuando no hay mas movimientos
 
@@ -25,16 +24,16 @@ saltoEnPosicion(Tablero,N,X,Y,solucion([HS|TS],X,Y,[norte|TM])) :-
 
 saltoEnPosicion(Tablero,N,X,Y,solucion([HS|TS],X,Y,[sur|TM])) :- 
 												X1 is X+1,
-												numFilas(Tablero,Tam),
-												X1 =< Tam,
+												length(Tablero,Tam),
+												X1 < Tam,
 												N1 is N-1,
 												saltoEnPosicion(Tablero,N1,X1,Y,solucion(TS,X1,Y,TM)),
 												numIguales(Tablero,X,Y,HS).
 
 saltoEnPosicion(Tablero,N,X,Y,solucion([HS|TS],X,Y,[este|TM])) :- 
 												Y1 is Y+1,
-												length(Tablero,Tam),
-												Y1 =< Tam,
+												numColums(Tablero,Tam),
+												Y1 < Tam,
 												N1 is N-1,
 												saltoEnPosicion(Tablero,N1,X,Y1,solucion(TS,X,Y1,TM)),
 												numIguales(Tablero,X,Y,HS).
